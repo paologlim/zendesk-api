ad42a181ff3b3177b6f24ba4b7a85c98a1b8d91d
Paolo G. Lim <paolo@caresharing.eu>
2011-03-30 13:49:42 +0800

    me thinks autoload is better than require

--- lib/zendesk-api.rb
+++ lib/zendesk-api.rb
@@ -7,19 +7,21 @@ require 'active_support/version'
 # need to pull in the pieces we want with Rails 3
 require 'active_support/core_ext' if ActiveSupport::VERSION::MAJOR == 3
 
+ZENDESK_LIB_DIR = File.dirname(__FILE__) unless defined?(ZENDESK_LIB_DIR)
+
 module Zendesk
   class Error < StandardError; end
   class CouldNotAuthenticateYou < StandardError; end
-end
 
-require 'zendesk/user'
-require 'zendesk/user_identity'
-require 'zendesk/organization'
-require 'zendesk/group'
-require 'zendesk/ticket'
-require 'zendesk/attachment'
-require 'zendesk/tag'
-require 'zendesk/forum'
-require 'zendesk/entry'
-require 'zendesk/search'
-require 'zendesk/main'
+  autoload :User, File.join(ZENDESK_LIB_DIR, 'zendesk/user')
+  autoload :UserIdentity, File.join(ZENDESK_LIB_DIR, 'zendesk/user_identity')
+  autoload :Organization, File.join(ZENDESK_LIB_DIR, 'zendesk/organization')
+  autoload :Group, File.join(ZENDESK_LIB_DIR, 'zendesk/group')
+  autoload :Ticket, File.join(ZENDESK_LIB_DIR, 'zendesk/ticket')
+  autoload :Attachment, File.join(ZENDESK_LIB_DIR, 'zendesk/attachment')
+  autoload :Tag, File.join(ZENDESK_LIB_DIR, 'zendesk/tag')
+  autoload :Forum, File.join(ZENDESK_LIB_DIR, 'zendesk/forum')
+  autoload :Entry, File.join(ZENDESK_LIB_DIR, 'zendesk/entry')
+  autoload :Search, File.join(ZENDESK_LIB_DIR, 'zendesk/search')
+  autoload :Main, File.join(ZENDESK_LIB_DIR, 'zendesk/main')
+end
